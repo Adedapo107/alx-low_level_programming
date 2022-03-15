1b64793be7156f889cab737a5d9781a78988d792
Abdulmaleek Olaifa <100152296+Adedapo107@users.noreply.github.com>
2022-03-15 21:29:44 +0100

    Update 0-putchar.c

--- 0x02_funtions_nested_loops/0-putchar.c
+++ 0x02_funtions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
 * main - Entry point
-* write a program tha prints Holberton, followed by a new line.
-* Return: Always (Success)
+* write a program tha prints _putchar, followed by a new line.
+* Return: Always 0 (Success)
 */
 int main(void)
 {
