de7a49e69e964ab1c24399ab6d79e86f1408d093
Abdulmaleek Olaifa <100152296+Adedapo107@users.noreply.github.com>
2022-04-07 18:33:44 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,51 @@
-#include "holberton.h"
-#include <stdlib.h>
+#include "main.h"
+
 /**
- * *string_nconcat - concatenates two strings
+ * string_nconcat - concatenates two strings.
  * @s1: first string
  * @s2: second string
- * @n: limit of s2
- * Return: pointer to new space in memory or null
- **/
+ * @n: index
+ * Return: char pointer
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *strDup;
-	int i;
-	unsigned int j;
+	char *p;
+	unsigned int size1 = 0, size2 = 0, i;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	i = 0;
-	while (s1[i] != '\0')
-		i++;
-	strDup = malloc(sizeof(char) * (i + n + 1));
-	if (strDup == NULL)
-		return (NULL);
-	i = j = 0;
-	while (s1[i] != '\0')
+
+	while (s1[size1] != '\0')
 	{
-		strDup[i] = s1[i];
-		i++;
+		size1++;
 	}
-	while (j < n && s2[j] != '\0')
+
+	while (s2[size2] != '\0')
 	{
-		strDup[i] = s2[j];
-		i++, j++;
+		size2++;
 	}
-	strDup[i] = '\0';
-	return (strDup);
+
+	if (n > size2)
+	n = size2;
+	p = malloc((size1 + n + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (0);
+
+	for (i = 0; i < size1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (; i < (size1 + n); i++)
+	{
+		p[i] = s2[i - size1];
+	}
+	p[i] = '\0';
+
+return (p);
 }
