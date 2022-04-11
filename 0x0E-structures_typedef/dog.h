7f1e52d5a59d166050e6424b780a8bdb42cb20e8
Abdulmaleek Olaifa <100152296+Adedapo107@users.noreply.github.com>
2022-04-11 21:33:38 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,7 +11,7 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
