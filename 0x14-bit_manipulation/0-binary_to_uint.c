de9deb0a82a3a93f9aba9ec68806e54297aae92c
Abdulmaleek Olaifa <100152296+Adedapo107@users.noreply.github.com>
2022-04-28 20:57:54 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,21 +1,47 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converst binary to uint
- * @b: binary number
- * Return: converted uint
+ * _stoi - converts chars to ints
+ * @c: char to convert
+ * Return: converted int
+ */
+unsigned int _stoi(char c)
+{
+	return ((unsigned int) c - '0');
+}
+/**
+ * _strlen - calculates the length of the string
+ * @s: input
+ * Return: length of string
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+/**
+ * binary_to_uint - converts a string of 1's and 0's to a decimal number
+ * @b: string to convert
+ * Return: unsigned decimal number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	uint a = 0;
+	int i;
+	unsigned int result, tmp, expo;
 
 	if (!b)
 		return (0);
-	while (*b)
+	result = tmp = 0;
+	expo = 1;
+	for (i = _strlen(b) - 1; b[i]; i--, expo *= 2)
 	{
-		if (*b != '1' && *b != '0')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		a = a * 2 + (*b++ - 48);
+		tmp = _stoi(b[i]);
+		result += tmp * expo;
 	}
-	return (a);
+	return (result);
 }
