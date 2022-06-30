e6abcaf9fd3284e3b14e03b0ff587a24486582a9
Abdulmaleek Olaifa <100152296+Adedapo107@users.noreply.github.com>
2022-06-30 13:45:26 +0100

    Update 5-main.c

--- 0x1A-hash_tables/5-main.c
+++ 0x1A-hash_tables/5-main.c
@@ -16,10 +16,10 @@ int main(void)
 	hash_table_print(ht);
 	hash_table_set(ht, "c", "fun");
 	hash_table_set(ht, "python", "awesome");
-	hash_table_set(ht, "Jennie", "and Jay love asm");
+	hash_table_set(ht, "Bob", "and Kris love asm");
 	hash_table_set(ht, "N", "queens");
 	hash_table_set(ht, "Asterix", "Obelix");
-	hash_table_set(ht, "Betty", "Holberton");
+	hash_table_set(ht, "Betty", "Cool");
 	hash_table_set(ht, "98", "Battery Street");
 	hash_table_print(ht);
 	return (EXIT_SUCCESS);
